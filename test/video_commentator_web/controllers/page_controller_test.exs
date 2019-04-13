fda827d60755b75758dee06e2e8279e20d1ec3f2
Barry Anderson <barryanderson@users.noreply.github.com>
2019-04-13 20:59:32 +0100

    Updated broken test

--- test/video_commentator_web/controllers/page_controller_test.exs
+++ test/video_commentator_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule VideoCommentatorWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Video Commentator!"
   end
 end

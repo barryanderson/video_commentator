8206752451d1f7b50624863768069442d609c751
Barry Anderson <barryanderson@users.noreply.github.com>
2019-04-13 20:50:28 +0100

    Added user create and new routes

--- lib/video_commentator_web/router.ex
+++ lib/video_commentator_web/router.ex
@@ -16,9 +16,8 @@ defmodule VideoCommentatorWeb.Router do
   scope "/", VideoCommentatorWeb do
     pipe_through :browser
 
-    get "/users", UserController, :index
-    get "/users/:id", UserController, :show
     get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
   end
 
   # Other scopes may use custom stacks.

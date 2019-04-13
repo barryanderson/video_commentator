df5acf41a2300a228c118fb6449418ed3e252022
Barry Anderson <barryanderson@users.noreply.github.com>
2019-04-13 20:01:06 +0100

    Add user controller routes

--- lib/video_commentator_web/router.ex
+++ lib/video_commentator_web/router.ex
@@ -16,6 +16,8 @@ defmodule VideoCommentatorWeb.Router do
   scope "/", VideoCommentatorWeb do
     pipe_through :browser
 
+    get "/users", UserController, :index
+    get "/users/:id", UserController, :show
     get "/", PageController, :index
   end
 

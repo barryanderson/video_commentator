ecc9152e26c9c44c20a049577770df2069b3032d
Barry Anderson <barryanderson@users.noreply.github.com>
2019-05-04 11:38:35 +0100

    Finxed unsued variable

--- lib/video_commentator_web/controllers/video_controller.ex
+++ lib/video_commentator_web/controllers/video_controller.ex
@@ -23,7 +23,7 @@ defmodule VideoCommentatorWeb.VideoController do
 
   def create(conn, %{"video" => video_params}, current_user) do
     case Multimedia.create_video(current_user, video_params) do
-      {:ok, video} ->
+      {:ok, _video} ->
         conn
         |> put_flash(:info, "Video created successfully.")
         |> redirect(to: Routes.video_path(conn, :index))
